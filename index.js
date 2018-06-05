3c884e76e425253248d2f738bde693ee5802075c
fedebabrauskas <fedebabrauskas@gmail.com>
2018-06-05 03:45:42 -0300

    added automatic next match announcer

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 import Discord from 'discord.js';
-import moment from 'moment';
-import { HLTV } from 'hltv';
 
-import { getNextMatch, getMatchStats } from './modules/hltv';
-import { prefix, token, myTeams } from './config';
+import { nextMatchDetails, nextMatchAnnouncer } from './modules/hltv';
+import { prefix, token } from './config';
 
 const client = new Discord.Client();
 
 client.on('message', msg => {
   const { content } = msg;
+
   if (!content.startsWith(prefix)) return;
 
   const args = content.slice(prefix.length).split(' ');
   const command = args.shift().toLowerCase();
 
-  // !nextmatch command
-  if (command === 'nextmatch') return getNextMatch(msg, myTeams);
-
-  // !match command
-  if (command === 'match')
-    return getMatchStats(msg, !args[1] ? args[0] : args.join(' '));
+  if (command === 'nextmatch') return nextMatchDetails(msg);
 });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
+  nextMatchAnnouncer(client);
 });
 
 client.login(token);
--- modules/hltv.js
+++ modules/hltv.js
@@ -1,84 +1,83 @@
 import Discord from 'discord.js';
-import { HLTV } from 'hltv';
 import moment from 'moment';
+import _ from 'lodash';
+import { HLTV } from 'hltv';
 
-export const getNextMatch = async (msg, myTeams) => {
+import { myTeams } from '../config';
+
+const getNextMatch = async () => {
   try {
-    // get all matches from HLTV
+    // get all matches from HLTV.org
     const allMatches = await HLTV.getMatches();
-    // create new array with my prefered matches based on teams array
-    const myMatches = allMatches.filter(({ team1, team2 }) => {
+    // create new array with matches of myTeams
+    const id = allMatches.filter(({ team1, team2, date }) => {
       // check if both teams exist
-      if (team1 && team2) {
+      if (team1 && team2)
         return (
-          myTeams.indexOf(team1.name) > -1 || myTeams.indexOf(team2.name) > -1
+          (date && myTeams.indexOf(team1.name) > -1) ||
+          myTeams.indexOf(team2.name) > -1
         );
-      }
-    });
-    // select first match from myMatches which has date property (isnt currently playing)
-    const { team1, team2, date, format, event, maps, live } = myMatches.filter(
-      match => match.date
-    )[0];
-    // create discord rich embed
-    const matchEmbed = new Discord.RichEmbed()
-      .setTitle(`${team1.name} vs. ${team2.name} ${live ? '(PLAYING)' : ''}`)
-      .setColor('#44bbfc')
-      .addField('Date', moment(new Date(date)).format('LLL'))
-      .addField('Format', format)
-      .addField('Event', event.name)
-      .addField('Maps', maps ? maps.map(m => m.name).join(', ') : 'TBA');
-    // send embed to the current channel
-    return msg.channel.send(matchEmbed);
+    })[0].id;
+    return await HLTV.getMatch({ id });
   } catch (err) {
     console.log(err);
   }
 };
 
-export const getMatchStats = async (msg, team) => {
-  try {
-    // get all matches from HLTV
-    const allMatches = await HLTV.getMatches();
-    // get the first match based on my request
-    const foundMatch = allMatches.filter(({ team1, team2 }) => {
-      if (team1 && team2) {
-        return team1.name == team || team2.name == team;
+export const nextMatchDetails = async msg => {
+  const {
+    team1,
+    team2,
+    date,
+    format,
+    event,
+    maps,
+    live,
+    streams
+  } = await getNextMatch();
+  // create discord rich embed
+  const matchEmbed = new Discord.RichEmbed()
+    .setTitle(`${team1.name} vs. ${team2.name} ${live ? '(PLAYING)' : ''}`)
+    .setColor('#44bbfc')
+    .addField('Date', moment(new Date(date)).format('LLL'))
+    .addField('Format', format)
+    .addField('Event', event.name)
+    .addField('Maps', maps.map(m => `${m.name} - ${m.result}`).join(', '))
+    .addField(
+      'Streams',
+      streams.length > 0
+        ? `${streams[0].name} (${streams[0].viewers}) - ${streams[0].link}`
+        : 'No streams for this match'
+    );
+  // print embed in the chat
+  return msg.channel.send(matchEmbed);
+};
+
+export const nextMatchAnnouncer = client => {
+  let timeInMinutes = 30;
+  // empty variable for current match
+  let currentMatch = {};
+  // find channel on every guild to print messages
+  client.guilds.forEach(g => {
+    let defaultChannel = '';
+    g.channels.forEach(c => {
+      if (c.type === 'text' && defaultChannel === '') {
+        if (c.permissionsFor(g.me).has('SEND_MESSAGES')) {
+          defaultChannel = c;
+        }
       }
-    })[0];
-
-    if (foundMatch) {
-      // get full stats from the match with the found match id
-      const {
-        team1,
-        team2,
-        date,
-        format,
-        event,
-        maps,
-        streams,
-        live
-      } = await HLTV.getMatch({ id: foundMatch.id });
-      // create the rich embed
-      const matchEmbed = new Discord.RichEmbed()
-        .setTitle(`${team1.name} vs. ${team2.name} ${live ? '(PLAYING)' : ''}`)
-        .setColor('#44bbfc')
-        .addField('Date', moment(new Date(date)).format('LLL'))
-        .addField('Format', format)
-        .addField('Event', event.name)
-        .addField('Maps', maps.map(m => `${m.name} - ${m.result}`).join(', '))
-        .addField(
-          'Streams',
-          streams.length > 0
-            ? `${streams[0].name} (${streams[0].viewers}) - ${streams[0].link}`
-            : 'No streams for this match'
+    });
+    setInterval(async () => {
+      const nextMatch = await getNextMatch();
+      const { team1, team2, date, streams } = nextMatch;
+      if (!_.isEqual(currentMatch, nextMatch)) {
+        currentMatch = nextMatch;
+        defaultChannel.send(
+          `**${team1.name} vs. ${team2.name}** - *${moment(
+            new Date(date)
+          ).calendar()}* ${!_.isEmpty(streams) ? `\n${streams[0].link}` : ''}`
         );
-
-      // send embed to the current channel
-      msg.channel.send(matchEmbed);
-    } else {
-      // if match isnt valid, return error
-      return msg.channel.send(`There are no matches for ${team}`);
-    }
-  } catch (err) {
-    console.log(err);
-  }
+      }
+    }, timeInMinutes * 60 * 1000);
+  });
 };
